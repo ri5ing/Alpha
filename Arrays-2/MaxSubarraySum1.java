24ddcaedd1625e6a252020c842a7d815534a3664
ri5ing <uday.patil.st@gmail.com>
2026-07-07 18:23:49 +0530

    Max Subarray Sum - 1 (Brute Force)

--- Arrays-2/MaxSubarraySum1.java
+++ Arrays-2/MaxSubarraySum1.java
@@ -1,10 +1,8 @@
 public class MaxSubarraySum1 {
     
-    public static void maxSubaarySum(int numbers[]){
+    public static void maxSubarraySum(int numbers[]){
         int currSum = 0;
         int maxSum = Integer.MIN_VALUE;
-
-
         for(int i=0 ; i<numbers.length ; i++){
             int start = i;
             for (int j=i ; j < numbers.length ; j++ ){
@@ -23,7 +21,7 @@ public class MaxSubarraySum1 {
     }
     public static void main(String[] args) {
         int numbers[] = {1,-2,6,-1,3};
-        maxSubaarySum(numbers);
+        maxSubarraySum(numbers);
     }
     
 }
---
+++ Arrays-2/MaxSubarraySum2.java
@@ -0,0 +1,4 @@
+public class MaxSubarraySum2 {
+    
+    
+}
